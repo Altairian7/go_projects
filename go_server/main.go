a16e81cf611634c99b6be2c2ad11ed1398d1e1f1
altairian7 <artisharma.as12.as@gmail.com>
2025-04-07 14:38:42 +0530

    made some changes in formHandler

--- go_server/main.go
+++ go_server/main.go
@@ -9,20 +9,17 @@ import (
 
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/form" {
-		http.Error(w, "404 not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "POST" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	r.ParseForm()
-	fmt.Fprintf(w, "POST request successful\n")
-	fmt.Fprintf(w, "Name: %s\n", r.FormValue("name"))
-	fmt.Fprintf(w, "Age: %s\n", r.FormValue("age"))
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return	
 }	
+	fmt.Fprintf(w, "POST request successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 if r.URL.Path != "/hello" {
