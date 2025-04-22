58d8346586a56a81b81caad0a90e3823e4838c7a
altairian7 <artisharma.as12.as@gmail.com>
2025-04-22 22:26:36 +0530

    generated code for gqlgen

--- simple-graphql-go/graph/resolver.go
+++ simple-graphql-go/graph/resolver.go
@@ -1,21 +1,45 @@
 package graph
 
+// THIS CODE WILL BE UPDATED WITH SCHEMA CHANGES. PREVIOUS IMPLEMENTATION FOR SCHEMA CHANGES WILL BE KEPT IN THE COMMENT SECTION. IMPLEMENTATION FOR UNCHANGED SCHEMA WILL BE KEPT.
+
 import (
 	"context"
-	"strconv"
 )
 
-// Resolver struct to hold in-memory data
-type Resolver struct {
-	users []*User
+type Resolver struct{}
+
+// CreateUser is the resolver for the createUser field.
+func (r *mutationResolver) CreateUser(ctx context.Context, name string) (*User, error) {
+	panic("not implemented")
 }
 
-// Query Resolver
+// Users is the resolver for the users field.
+func (r *queryResolver) Users(ctx context.Context) ([]*User, error) {
+	panic("not implemented")
+}
+
+// Mutation returns MutationResolver implementation.
+func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
+
+// Query returns QueryResolver implementation.
+func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+/*
+	type Resolver struct {
+	users []*User
+}
 func (r *Resolver) Users(ctx context.Context) ([]*User, error) {
 	return r.users, nil
 }
-
-// Mutation Resolver
 func (r *Resolver) CreateUser(ctx context.Context, name string) (*User, error) {
 	user := &User{
 		ID:   strconv.Itoa(len(r.users) + 1),
@@ -24,3 +48,4 @@ func (r *Resolver) CreateUser(ctx context.Context, name string) (*User, error) {
 	r.users = append(r.users, user)
 	return user, nil
 }
+*/
