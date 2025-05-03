a189b5b49eedb7288d6d7f3626b09d34253d7e97
altairian7 <artisharma.as12.as@gmail.com>
2025-05-04 00:03:20 +0530

    created init and shutdown func

--- simple-graphql-go/main.go
+++ simple-graphql-go/main.go
@@ -20,3 +20,14 @@ func main() {
 	log.Println("Server running at http://localhost:8080/")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+func init() {
+	log.Println("Initializing server...")
+	log.Println("Server initialized successfully.")
+}
+
+func shutdown() {
+	log.Println("Shutting down server...")
+	log.Println("Server shut down successfully.")
+	log.Println("Goodbye!")
+}
