ca7fb50b1650b115f27aecf5c3235e9450c47f77
altairian7 <artisharma.as12.as@gmail.com>
2025-05-14 15:28:46 +0530

    added user model

---
+++ javaVirtualBank/models/user.java
@@ -0,0 +1,19 @@
+package models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String username;
+    private String password;
+    private String fullName;
+
+    public User(String username, String password, String fullName) {
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+    }
+
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getFullName() { return fullName; }
+}
